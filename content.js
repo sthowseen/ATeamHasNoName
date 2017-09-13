da371a85d4516d8b494344e64a79d7d00b98fab1
Shakir Thowseen <sthowseen@spokeo.com>
2017-09-13 13:57:55 -0700

    first commit

---
+++ content.js
@@ -0,0 +1,43 @@
+'use strict';
+
+InboxSDK.load(2, 'sdk_shakirthow_df46724836').then(sdk => {
+	sdk.Conversations.registerThreadViewHandler(threadView => {
+		const el = document.createElement("div");
+		el.innerHTML = 'A Team with no name';
+
+		threadView.addSidebarContentPanel({
+			title: 'Sidebar Example',
+			iconUrl: chrome.runtime.getURL('monkey.png'),
+			el
+		});
+	});
+
+
+
+	sdk.Lists.registerThreadRowViewHandler(threadRowView =>{
+
+		var contacts = threadRowView.getContacts();
+		for (var i = 0; i < contacts.length; i++) {
+			var contact = contacts[i];
+      // getStripeCustomerWithoutMyDomain(contact, sdk.User.getEmailAddress()).then(function(customer) {
+
+					threadRowView.addImage({
+						imageUrl: chrome.runtime.getURL('monkey.png'),
+						tooltip: "email",
+						imageClass: "rounded_stripe"
+					});
+
+				
+			// });
+		}
+
+
+
+
+
+
+
+
+	})
+
+});
