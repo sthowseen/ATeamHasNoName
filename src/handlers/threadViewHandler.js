e267c87855b7f4becc4a5c4155d69ca3ef1fbcda
Shakir Thowseen <sthowseen@spokeo.com>
2017-09-14 14:13:19 -0700

    placeholder ajax

--- src/handlers/threadViewHandler.js
+++ src/handlers/threadViewHandler.js
@@ -2,17 +2,8 @@ import { getAssetUrl } from "../utils";
 
 export default threadView => {
   const el = document.createElement("div");
-  el.className = 'spokeo-sidebar'
+  el.className = "spokeo-sidebar";
   el.innerHTML = "A Team with no name";
-
-  var views = threadView.getMessageViews();
-
-  views.forEach(function(view) {
-    var sender = view.getSender();
-    var recipients = view.getRecipients();
-    console.log(sender);
-    console.log(recipients);
-  });
 
   var sideBar = threadView.addSidebarContentPanel({
     title: "Sidebar Exampleee",
@@ -20,17 +11,34 @@ export default threadView => {
     el
   });
 
+  var views = threadView.getMessageViews();
+  var users = [];
+  views.forEach(function(view) {
+    var sender = view.getSender();
+    var recipients = view.getRecipients();
+    users.push(sender);
+    users = users.concat(recipients);
+  });
+
+  fetchProfiles(users);
+
   //Hack to force display side bar
-  var sideBarEl = $(`[data-sdk-sidebar-instance-id='${sideBar._contentPanelViewImplementation._sidebarId}']`)[0];
-  $(sideBarEl).first().css( "display", "block !important" );
-
-  
-
-  //   getStripeCustomerWithoutMyDomain(contact, sdk.User.getEmailAddress()).then(function(customer) {
-  // 						threadRowView.addImage({
-  // 							imageUrl: chrome.runtime.getURL('monkey.png'),
-  // 							tooltip: "email",
-  // 							imageClass: "rounded_stripe"
-  // 						});
-  // 				});
+  var sideBarEl = $(
+    `[data-sdk-sidebar-instance-id='${sideBar._contentPanelViewImplementation
+      ._sidebarId}']`
+  )[0];
+  $(sideBarEl)
+    .first()
+    .css("display", "block !important");
 };
+
+function updateUI(resp) {
+  console.log(resp);
+  $(".spokeo-sidebar").html(JSON.stringify(resp));
+}
+
+function fetchProfiles(users) {
+  console.log("****USERS****");
+  console.log(users);
+  $.get("https://jsonplaceholder.typicode.com/users", updateUI);
+}
