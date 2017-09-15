e8540daf26a52acccc5a0da09d08e9adcd410edd
Shakir Thowseen <sthowseen@spokeo.com>
2017-09-15 10:22:43 -0700

    add icon to connections

--- src/connections/connectionsRouteView.js
+++ src/connections/connectionsRouteView.js
@@ -1,24 +1,25 @@
 // weird stuff here to get around a chrome bug with content security policies (CSP)
 // see https://stackoverflow.com/questions/24641592/injecting-iframe-into-page-with-restrictive-content-security-policy
+import { getAssetUrl } from "../utils";
 
 export function connectionsRouteViewHandler(connectionsRouteView) {
   let el = connectionsRouteView.getElement();
 
   // Avoid recursive frame insertion...
-  var extensionOrigin = 'chrome-extension://' + chrome.runtime.id;
+  var extensionOrigin = "chrome-extension://" + chrome.runtime.id;
   if (!location.ancestorOrigins.contains(extensionOrigin)) {
-    var iframe = document.createElement('iframe');
-    iframe.src = chrome.runtime.getURL('frame-connections.html');
+    var iframe = document.createElement("iframe");
+    iframe.src = chrome.runtime.getURL("frame-connections.html");
     iframe.style.cssText =
-      'position:relative;top:0;left:0;display:block;width:100%;height:100%;z-index:1000;border:0;';
+      "position:relative;top:0;left:0;display:block;width:100%;height:100%;z-index:1000;border:0;";
     el.appendChild(iframe);
   }
 }
 
 export function createConnectionsNavItem(routeId) {
   return {
-    name: 'Spokeo Connections',
-    iconUrl: '',
+    name: "Connections",
+    iconUrl: getAssetUrl("assets/Logo-Color.png"),
     routeID: routeId
   };
 }
