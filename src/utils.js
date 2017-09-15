320052035b502c313215b3aec40a919e976ded8c
Shakir Thowseen <sthowseen@spokeo.com>
2017-09-15 07:38:44 -0700

    bug fix

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,6 @@
 export function getAssetUrl(assetPath) {
   // TODO: detect if local or remote assets are needed
-  return chrome.runtime.getURL(assetPath)
+  return chrome.runtime.getURL(assetPath);
 }
 
 export function createContact(person) {
@@ -10,7 +10,7 @@ export function createContact(person) {
     location: getLocation(person),
     avatar: getAvatar(person),
     usernames: getUsernames(person)
-  }
+  };
 }
 
 function getName(person) {
@@ -22,26 +22,45 @@ function getName(person) {
 }
 
 function getLocation(person) {
-  return person.aggregate_info.location || '';
+  return person.aggregate_info.location || "";
 }
 
 function getAvatar(person) {
   return (
     (person.aggregate_info.profile_photo &&
       person.aggregate_info.profile_photo.src) ||
-    'https://pbs.twimg.com/profile_images/477397164453527552/uh2w1u1o_400x400.jpeg'
+    "https://pbs.twimg.com/profile_images/477397164453527552/uh2w1u1o_400x400.jpeg"
   );
 }
 
 function getUsernames(person) {
-  if (person.username_sources.length > 0) { 
+  console.log("*******");
+  console.log(person);
+  if (person.username_sources.length > 0) {
     return [
-      {username: person.username_sources[0].username || '', avatar: person.username_sources[0].profile_photo.src || ''},
-      {username: person.username_sources[1].username || '', avatar: person.username_sources[1].profile_photo.src || ''}
-    ]
+      {
+        username: person.username_sources[0].username || "",
+        avatar: getPic(person.username_sources[0]) || ""
+      },
+      {
+        username: person.username_sources[1].username || "",
+        avatar: getPic(person.username_sources[0]) || ""
+      }
+    ];
   } else {
-    return [{
-      username: 'coolguy', avatar: getAvatar(person)
-    }];
+    return [
+      {
+        username: "coolguy",
+        avatar: getAvatar(person)
+      }
+    ];
+  }
+}
+
+function getPic(data) {
+  if (data.profile_photo) {
+    return data.profile_photo.src;
+  } else {
+    return "";
   }
 }
